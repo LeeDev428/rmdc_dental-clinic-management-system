3b8293e2cd81aea0555424589839bed8cd2e0efe
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-04 02:39:49 +0800

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,6 +12,7 @@
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
         <link href="{{ asset('admin/css/styles.css')}}" rel="stylesheet" />
+        <link href="{{ asset('admin/css/custom.css')}}" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
