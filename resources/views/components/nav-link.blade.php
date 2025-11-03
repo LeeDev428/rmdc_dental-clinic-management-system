015756d4ad424408330fc7c2935aa2bce195d567
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-04 07:28:21 +0800

    Update nav-link.blade.php

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
-@props(['active'])
+@props(['active' => false])
 
 @php
-$classes = $active
+$classes = $active ?? false
     ? 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 dark:text-white hover:text-gray-950 focus:outline-none transition duration-150 ease-in-out'
     : 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-500 dark:text-gray-500 hover:text-gray-950 focus:outline-none transition duration-150 ease-in-out';
 @endphp
