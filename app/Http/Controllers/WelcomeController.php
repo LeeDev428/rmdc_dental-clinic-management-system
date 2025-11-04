4a72040b523bc2b793fdedfc0307f222c70d9991
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-04 22:08:42 +0800

    Update WelcomeController.php

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,4 +12,19 @@ class WelcomeController extends Controller
         $procedures = ProcedurePrice::all(); // Fetch all dental procedures
         return view('welcome', compact('procedures'));
     }
+    
+    public function getServices(Request $request)
+    {
+        $page = $request->input('page', 1);
+        $perPage = 12;
+        
+        $procedures = ProcedurePrice::paginate($perPage);
+        
+        return response()->json([
+            'data' => $procedures->items(),
+            'current_page' => $procedures->currentPage(),
+            'last_page' => $procedures->lastPage(),
+            'total' => $procedures->total()
+        ]);
+    }
 }
