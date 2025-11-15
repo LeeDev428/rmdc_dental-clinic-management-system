1384709a3d62df882d977cbec56607e199d12b4d
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-16 07:51:49 +0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -378,7 +378,7 @@ function formatDate(dateString) {
     <!-- Profile Button -->
     <button class="inline-flex items-center px-7 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-white bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150" @click="open = !open">
         <img src="{{ Auth::user()->avatar_url }}" alt="Avatar" class="w-8 h-8 rounded-full">
-        <div class="ms-2 hidden sm:block dark:text-white">{{ Auth::user()->name }}</div>
+        <div class="ms-2 hidden sm:block dark:text-white">{{ Str::limit(Auth::user()->name, 10, '...') }}</div>
         <div class="ms-1">
             <svg class="fill-current h-4 w-4 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 111.414 1.414l-4 4a1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -413,10 +413,13 @@ function formatDate(dateString) {
 
 @vite('resources/js/app.js')
 <script>
-    Echo.channel('appointments')
-        .listen('AppointmentStatusChanged', (e) => {
-            fetchNotifications();
-        });
+    // Check if Echo is available before using it
+    if (typeof Echo !== 'undefined') {
+        Echo.channel('appointments')
+            .listen('AppointmentStatusChanged', (e) => {
+                fetchNotifications();
+            });
+    }
 
     const fetchNotifications = async () => {
         try {
