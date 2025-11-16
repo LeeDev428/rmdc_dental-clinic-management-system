8d16810be1e17df73bca851a7f79f815076c6429
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-16 08:49:12 +0800

    Update ProcedurePrice.php

--- app/Models/ProcedurePrice.php
+++ app/Models/ProcedurePrice.php
@@ -12,5 +12,23 @@ class ProcedurePrice extends Model
     protected $table = 'procedure_prices';
 
     protected $fillable = ['procedure_name', 'price', 'duration', 'image_path', 'description'];
+
+    /**
+     * Get the inventory links for this procedure
+     */
+    public function procedureInventories()
+    {
+        return $this->hasMany(ProcedureInventory::class);
+    }
+
+    /**
+     * Get the inventory items used in this procedure (many-to-many)
+     */
+    public function inventories()
+    {
+        return $this->belongsToMany(Inventory::class, 'procedure_inventory')
+                    ->withPivot('quantity_used')
+                    ->withTimestamps();
+    }
 }
  
