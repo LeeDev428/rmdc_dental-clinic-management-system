9a7bb646afea1809c4012e27f90b4781ecb3dea0
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-16 08:48:47 +0800

    Update ProcedureInventory.php

--- app/Models/ProcedureInventory.php
+++ app/Models/ProcedureInventory.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProcedureInventory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'procedure_inventory';
+
+    protected $fillable = [
+        'procedure_price_id',
+        'inventory_id',
+        'quantity_used'
+    ];
+
+    protected $casts = [
+        'quantity_used' => 'decimal:2'
+    ];
+
+    /**
+     * Get the procedure that owns this inventory link
+     */
+    public function procedurePrice()
+    {
+        return $this->belongsTo(ProcedurePrice::class);
+    }
+
+    /**
+     * Get the inventory item
+     */
+    public function inventory()
+    {
+        return $this->belongsTo(Inventory::class);
+    }
 }
