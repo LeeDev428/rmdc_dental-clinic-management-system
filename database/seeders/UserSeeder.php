676cfcf0bc530b2aea34468c185ede489ec375a0
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-19 21:45:19 +0800

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -29,7 +29,7 @@ class UserSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        // Create Staff User
+        // Create  User
         DB::table('users')->insert([
             'name' => 'user',
             'email' => 'user@gmail.com',
@@ -38,58 +38,6 @@ class UserSeeder extends Seeder
             'usertype' => 'user',
             'bio' => 'Front desk staff at RMDC Dental Clinic',
             'avatar' => 'img/default-dp.jpg',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-        // Create Doctor
-        DB::table('users')->insert([
-            'name' => 'Dr. Sarah Smith',
-            'email' => 'admin@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('admin123'),
-            'usertype' => 'admin',
-            'bio' => '',
-            'avatar' => 'img/default-dp.jpg',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-        // Create Regular Users
-        for ($i = 1; $i <= 10; $i++) {
-            DB::table('users')->insert([
-                'name' => 'User ' . $i,
-                'email' => 'user' . $i . '@example.com',
-                'email_verified_at' => now(),
-                'password' => Hash::make('password123'),
-                'usertype' => 'user',
-                'bio' => 'Regular clinic patient',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
-
-        // Create Social Auth Users
-        DB::table('users')->insert([
-            'name' => 'Google User',
-            'email' => 'google.user@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make(Str::random(24)),
-            'auth_provider' => 'google',
-            'auth_provider_id' => '12345678901234',
-            'usertype' => 'user',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'Facebook User',
-            'email' => 'facebook.user@example.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make(Str::random(24)),
-            'auth_provider' => 'facebook',
-            'auth_provider_id' => '987654321098',
-            'usertype' => 'user',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
