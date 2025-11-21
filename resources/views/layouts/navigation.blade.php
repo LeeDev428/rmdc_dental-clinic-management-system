f060ae5fdadc0156c97396ea80052999c63135d0
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-22 01:35:21 +0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -380,8 +380,8 @@ function formatDate(dateString) {
         <img src="{{ Auth::user()->avatar_url }}" alt="Avatar" class="w-8 h-8 rounded-full">
         <div class="ms-2 hidden sm:block dark:text-white">{{ Str::limit(Auth::user()->name, 10, '...') }}</div>
         <div class="ms-1">
-            <svg class="fill-current h-4 w-4 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" style="stroke-width: 3;">
-                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 111.414 1.414l-4 4a1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" stroke="currentColor" stroke-width="2.5" />
+            <svg class="fill-current h-4 w-4 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" >
+                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 111.414 1.414l-4 4a1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
             </svg>
         </div>
     </button>
