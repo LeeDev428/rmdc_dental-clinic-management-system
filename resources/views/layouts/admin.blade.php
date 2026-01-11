f8b92cd27ccfe5c98e5041c2e9ee4cf804681c97
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-11 15:12:09 +0800

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,9 +14,7 @@
         <link href="{{ asset('admin/css/styles.css')}}" rel="stylesheet" />
         <link href="{{ asset('admin/css/custom.css')}}" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
         <style>
             /* Background for login and registration pages */
