903ca16cc042261c0b3b0da6806f90c734831992
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-12 02:05:32 +0800

    Update sidebar.blade.php

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -33,14 +33,14 @@
             </a>
             <a class="nav-link" href="{{ route('admin.cancellation.requests') }}">
                 <div class="sb-nav-link-icon"><i class="fas fa-ban"></i></div>
-                Cancellation Requests
+                Cancels/Rescheds
                 <span id="sidebarCancellationBadge" class="badge bg-secondary ms-2">0</span>
             </a>
-            <a class="nav-link" href="{{ route('admin.reschedule.requests') }}">
+            {{-- <a class="nav-link" href="{{ route('admin.reschedule.requests') }}">
                 <div class="sb-nav-link-icon"><i class="fas fa-calendar-alt"></i></div>
                 Reschedule Requests
                 <span id="sidebarRescheduleBadge" class="badge bg-secondary ms-2">0</span>
-            </a>
+            </a> --}}
             {{-- <a class="nav-link" href="#">
                 <div class="sb-nav-link-icon"><i class="fas fa-edit"></i></div>
                 Edit Availability
