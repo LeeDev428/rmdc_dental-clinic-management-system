ecb0ec7141ed46626a3d6229fe9676813210edf4
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-11 15:12:00 +0800

    Update appointment-cancellation.blade.php

--- resources/views/appointment-cancellation.blade.php
+++ resources/views/appointment-cancellation.blade.php
@@ -176,7 +176,7 @@
                             <table class="min-w-full divide-y divide-gray-200">
                                 <thead class="bg-gray-50">
                                     <tr>
-                                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date Cancelled</th>
+                                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date</th>
                                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Appointment</th>
                                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Reason</th>
                                     </tr>
