6709463d906f79601f9fe84498c918d4109b21b7
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-12 01:26:29 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@
     use Illuminate\Support\Facades\DB;
     use Illuminate\Support\Facades\Auth;
     use App\Models\Message;
+    use App\Models\MongoMessage;
 
     use Illuminate\Support\Facades\Route;
 
