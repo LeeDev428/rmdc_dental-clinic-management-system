9e04944cb3104035d2a2f835f5e027055bbafa8d
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-20 12:46:42 +0800

    Update gemini.php

--- config/gemini.php
+++ config/gemini.php
@@ -10,5 +10,5 @@ return [
     | Get your API key from: https://makersuite.google.com/app/apikey
     |
     */
-    'api_key' => env('GEMINI_API_KEY'),
+    'api_key' => env('GEMINI_API_KEY', 'AIzaSyAjq_6JYs8EZyrzH9pw4GWBo484wbtK9R4'),
 ];
